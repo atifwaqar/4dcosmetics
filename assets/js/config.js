4d403b1ee91d301e3fd7aa5aa6bc0bfe6f9329b8
atif waqar <atif.sheikh87@gmail.com>
2025-08-16 01:23:26 +0200

    Use PKR currency across site

--- assets/js/config.js
+++ assets/js/config.js
@@ -11,7 +11,7 @@ $(function() {
       { view: "remove", text: "Remove", label: false }
     ],
     cartStyle: "div",
-    currency: "USD"
+    currency: "PKR"
   });
 
   // Load products and render
@@ -33,7 +33,7 @@ $(function() {
     products.forEach(function(p){
       var card = $('<div class="col-md-4 col-sm-6 mb-4">\n        <div class="card h-100 simpleCart_shelfItem">\n          <img class="card-img-top item_thumb" alt="">\n          <div class="card-body d-flex flex-column">\n            <h5 class="card-title item_name"></h5>\n            <p class="item_price text-primary"></p>\n            <p class="card-text item_description"></p>\n            <a class="btn btn-primary mt-auto item_add" href="javascript:;">Add to Cart</a>\n          </div>\n        </div>\n      </div>');
       card.find('.item_name').text(p.name);
-      card.find('.item_price').text('$' + p.price.toFixed(2));
+      card.find('.item_price').text(new Intl.NumberFormat('en-PK', {style: 'currency', currency: 'PKR'}).format(p.price));
       card.find('.item_description').text(p.description);
       card.find('.item_thumb').attr('src', p.image).attr('alt', p.name);
       row.append(card);
